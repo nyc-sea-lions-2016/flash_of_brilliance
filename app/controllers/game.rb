bd928e8c2893a378d838754a90ed09bc7ab3241b
Akeem Street & Alex Forger <pair+akeem-s.darrow87@devbootcamp.com>
2016-03-26 18:51:38 -0400

    Add Stats - Game Logic WORKSgit add .git add .

--- app/controllers/game.rb
+++ app/controllers/game.rb
@@ -23,38 +23,7 @@ post '/games' do
 end
 
 
-post '/games/:id' do
-  @user = User.find(session[:user_id])
-  @deck = Deck.find(params[:id])
-  idx = session[:indexes].first
-  if !session[:indexes].empty?
-    @card = Card.find(idx)
-    @game = Game.find(session[:game_id])
-    @guess = Guess.create(card_id: @card.id, game_id: session[:game_id])
-    # @game.increment!(:total_guesses)
-      if @card.answer == params[:answer]
-        session[:indexes].shift
-        # @guess.increment!(:guess_count)
-        # @game.increment!(:num_correct)
 
-        # if @guess.guess_count < 2
-        #   # @game.increment!(:first_try_count)
-        # end
-
-        erb :"games/show"
-      else
-        # @guess.increment!(:guess_count)
-        # @game.increment!(:num_incorrect)
-
-
-        erb :"games/show"
-      end
-
-  else
-    redirect "/users/#{session[:user_id]}"
-  end
-  # erb :'games/show'
-end
 
 
 
--- app/controllers/guess.rb
+++ app/controllers/guess.rb
@@ -1,13 +1,21 @@
  post '/games/:id/guesses' do
   @user = User.find(session[:user_id])
   @game = Game.find(params[:id])
+  session[:game_id] = @game.id
   idx = session[:indexes].first
   @card = Card.find(idx)
   @guess = Guess.create(card_id: @card.id, game_id: @game.id)
+  @game.increment!(:total_guesses)
   if @card.answer == params[:answer]
+    @guess.increment!(:guess_count)
+    @game.increment!(:num_correct)
+    if @guess.guess_count < 2
+       @game.increment!(:first_try_count)
+    end
     session[:indexes].shift
   else
-    #guess counter stuff
+    @guess.increment!(:guess_count)
+    @game.increment!(:num_incorrect)
   end
   if session[:indexes].empty?
     redirect "/users/#{@user.id}"
@@ -17,17 +25,9 @@
 
  end
 
-    # @game.increment!(:total_guesses)
-
-        # @guess.increment!(:guess_count)
-        # @game.increment!(:num_correct)
-
-        # if @guess.guess_count < 2
-        #   # @game.increment!(:first_try_count)
-        # end
 
 
-       # @guess.increment!(:guess_count)
-        # @game.increment!(:num_incorrect)
+
+
 
   # erb :'games/show'
